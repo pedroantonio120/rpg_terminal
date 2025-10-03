4d112d84ed58da34134dfbe04ebc5b51ac7c1ace
pedroantonio120 <gozzi.pedro@escola.pr.gov.br>
2025-10-03 11:15:17 -0300

    update

---
+++ exe3.py
@@ -0,0 +1,26 @@
+#Exericicos
+
+loja_comidas ={
+    'laranja baiana':{
+        'valor': 4.00,
+    },
+    'maca verde':{
+        'valor': 5.00,
+    },
+    'coco verde':{
+        'valor': 2.00,
+    },
+}
+
+for k,v in loja_comidas.items():
+  loja_comidas.get(k)['valor']*1.2
+print(loja_comidas)
+
+loja_comidas.update({'Mexerica': 4.00})
+print(loja_comidas,'#Mexirica adicionada')
+
+for chave, valor in loja_comidas.items():
+  print(f"key:{chave}: value:'{valor}")
+
+loja_comidas.pop('maca verde')
+print(loja_comidas, '#Maca verde removida')
